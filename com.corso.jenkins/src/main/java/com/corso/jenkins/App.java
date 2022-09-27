a524ecf7256f15affb628461c0e3690b2fa22231
Antonio Minelli <antonio.minelli.tech@gmail.com>
2022-09-27 16:58:32 +0200

    First Commit
    First Commit

---
+++ com.corso.jenkins/src/main/java/com/corso/jenkins/App.java
@@ -0,0 +1,16 @@
+package com.corso.jenkins;
+
+public class App {
+
+	public String play(int number) {
+		
+		if (number == 0) throw new IllegalArgumentException("Indicare un numero diverso da 0");
+		
+		if (number % 3 == 0) return "Divisibile per 3";
+		if (number % 5 == 0 ) return "Divisibile per 5";
+		
+		return String.valueOf(number);
+	}	
+	
+	
+}
---
+++ com.corso.jenkins/src/test/java/com/corso/jenkins/AppTest.java
@@ -0,0 +1,61 @@
+package com.corso.jenkins;
+
+ 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+		public App app;
+		
+		
+		@BeforeEach
+		public void setUp() {
+			app = new App();
+		}
+		
+		
+		@Test
+		@DisplayName("Play per number = 1")
+		public void testNumber1() {
+			String test = app.play(1);
+			Assertions.assertEquals(test, "1");			 
+		}
+		
+
+		@Test
+		@DisplayName("Play per number = 3")
+		public void testNumber2() {
+			String test = app.play(3);
+			Assertions.assertEquals(test, "Divisibile per 3");			 
+		}
+
+		
+		@Test
+		@DisplayName("Play per number = 5")
+		public void testNumber3() {
+			String test = app.play(5);
+			Assertions.assertEquals(test, "Divisibile per 5");			 
+		}
+		
+		@Test
+		@DisplayName("Play per number = 00")
+		public void testNumber4() {
+			Assertions.assertThrows(
+				IllegalArgumentException.class, 
+				() -> app.play(0)
+			);
+						 
+		}
+
+		
+		
+		@AfterEach
+		public void tearDown() {
+			app = null;
+		}
+	
+}
