7b03659c5186afbae6d3b1dc69bcae4cb75ce8cd
Antonio Minelli <antonio.minelli.tech@gmail.com>
2022-10-11 10:21:56 +0200

    update

--- com.corso.jenkins/src/main/java/com/corso/jenkins/App.java
+++ com.corso.jenkins/src/main/java/com/corso/jenkins/App.java
@@ -3,7 +3,7 @@ package com.corso.jenkins;
 public class App {
 
 	public String play(int number) {
-		
+		// Check su numero
 		if (number == 0) throw new IllegalArgumentException("Indicare un numero diverso da 0");
 		
 		if (number % 3 == 0) return "Divisibile per 3";
